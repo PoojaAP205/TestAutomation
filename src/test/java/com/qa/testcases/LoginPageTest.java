01276480c313e03f07fabd342d3a31ee0ff8e687
unknown <Pooja Nayar@PoojaN-D.int.asl.mum.sst>
2024-12-26 13:58:14 +0530

    Logintestpage changes commit

--- src/test/java/com/qa/testcases/LoginPageTest.java
+++ src/test/java/com/qa/testcases/LoginPageTest.java
@@ -1,5 +1,6 @@
 package com.qa.testcases;
 
+/*************************************** PURPOSE **********************************/
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
